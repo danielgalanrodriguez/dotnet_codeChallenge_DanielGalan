d97fa63f1d81f990bef6e899ba942f9e0fd1e4c3
Daniel Galán <danielgalanrodri@gmail.com>
2019-04-26 01:47:13 +0200

    Changed directory structure to add tests

---
+++ dotnet_codeChallenge_DanielGalan.Tests/UsersTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using dotnet_codeChallenge_DanielGalan.Models;
+
+namespace dotnet_codeChallenge_DanielGalan.Tests
+{
+    public class UsersTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void userRegister_registerOK_returns1()
+        {  // Arrange
+            var user = new Users();
+            user.email = "test@test.test";
+
+            //Act
+            var result = user.registerUser();
+            
+            //Assert
+            Assert.That( result=="1");
+        }
+
+        [Test]
+        public void updateUser_updateOK_returns1()
+        {  // Arrange
+            var user = new Users();
+            user.id = 1;
+            user.email="putTest@test.test";
+
+            //Act
+            var result = user.updateUser();
+            
+            //Assert
+            Assert.That( result=="1");
+        }
+    }
+}
